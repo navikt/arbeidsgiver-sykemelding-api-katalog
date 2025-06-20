4309e384b32b4cdf318aafa75e939bf5f9ea467a
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-06-20 14:11:20 +0200

    init

---
+++ postman/pre_request_script_maskinporten_token.js
@@ -0,0 +1,65 @@
+const jsonwebtoken = pm.require('npm:jsonwebtoken@8.5.1');
+const { v4: uuidv4 } = require('uuid');
+
+const privateKey = pm.environment.get("MASKINPORTEN_PRIVATE_KEY");
+const clientId = pm.environment.get("MASKINPORTEN_CLIENT_ID"); // Integration ID
+const kid = pm.environment.get("MASKINPORTEN_KID");
+const orgnr = pm.environment.get("customerOrgnr")
+
+if (!privateKey || !clientId || !kid || !orgnr) {
+  throw new Error("Fetching maskinporten token failed. Missing required environment variables: customerOrgnr, MASKINPORTEN_PRIVATE_KEY, MASKINPORTEN_CLIENT_ID, or MASKINPORTEN_KID");
+}
+
+// Generer JWT for systembruker ref: https://docs.digdir.no/docs/Maskinporten/maskinporten_func_systembruker.html#foresp%C3%B8rsel
+function generateJWT(orgnr) {
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    const payload = {
+        aud: "https://test.maskinporten.no/",
+        iss: clientId,
+        scope: "nav:helseytelser/sykepenger",
+        authorization_details: [{
+            type: "urn:altinn:systemuser",
+            systemuser_org: {
+                authority: "iso6523-actorid-upis",
+                ID: "0192:" + orgnr
+            }
+        }],
+        iat: currentTimestamp,
+        exp: currentTimestamp + 120,
+        jti: uuidv4()
+    };
+    const options = {
+        algorithm: 'RS256',
+        header: {
+            kid: kid
+        }
+    };
+
+    return jsonwebtoken.sign(payload, privateKey, options);
+}
+
+// Hent token med JWT ref: https://docs.digdir.no/docs/Maskinporten/maskinporten_protocol_token.html
+async function getMaskinportenToken(assertion) {
+  const res = await new Promise(r =>
+    pm.sendRequest({
+      url: 'https://test.maskinporten.no/token',
+      method: 'POST',
+      header: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: {
+        mode: 'urlencoded',
+        urlencoded: [
+          { key: 'grant_type', value: 'urn:ietf:params:oauth:grant-type:jwt-bearer' },
+          { key: 'assertion', value: assertion }
+        ]
+      }
+    }, (_, res) => r(res))
+  );
+    
+  if (res.code !== 200) throw new Error('Maskinporten token request failed: ' + res.code + "\n" + res.body);
+  return res.json().access_token;
+}
+
+const jwt = generateJWT(orgnr)
+const maskinportenToken = await getMaskinportenToken(jwt)
+pm.environment.set("bearerToken", maskinportenToken)
+console.log("Set bearerToken for maskinporten systemuser with delegated permissions for orgnr: " + orgnr)
